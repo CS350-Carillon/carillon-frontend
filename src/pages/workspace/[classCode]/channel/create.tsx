c1c80f0bdccad755ded1fda52b92e33e96a8449b
yumin-jung <jungym887@naver.com>
2023-05-29 15:39:15 +0900

    feat: Implement API in channelManage page

--- src/pages/workspace/[classCode]/channel/create.tsx
+++ src/pages/workspace/[classCode]/channel/create.tsx
@@ -1,5 +1,4 @@
-import '../../../../app/globals.css'
-import React from 'react'
+import React, { useState, ChangeEvent } from 'react'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Stack from '@mui/material/Stack'
@@ -9,18 +8,83 @@ import TextField from '@mui/material/TextField'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
-import ListItemAvatar from '@mui/material/ListItemAvatar'
-import Avatar from '@mui/material/Avatar'
+import { placeholder, localPort } from '@/utils/constants'
 import SearchBar from '@/components/SearchBar'
-import SideBar from '../../../../components/SideBar'
+import { IUser } from '@/utils/types'
+import { useRouter } from 'next/router'
+import SideBar from '@/components/SideBar'
 
-const dummyData = {
-  workspaceNamePlaceholder: 'Enter Channel Name',
-  descriptionPlaceholder: 'Enter Channel Description',
-  members: ['Mina', 'Whyojin', 'Erik'],
+interface UsersProps {
+  users: IUser[]
 }
 
-export default function Channel() {
+export default function Channel({ users }: UsersProps) {
+  const router = useRouter()
+  const [channelName, setChannelName] = useState('')
+  const [description, setDescription] = useState('')
+  const [, setSearchQuery] = useState('')
+  const [searchResults, setSearchResults] = useState<IUser[]>([])
+  const [selectedMembers, setSelectedMembers] = useState<IUser[]>([])
+
+  const handleChannelNameChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setChannelName(event.target.value)
+  }
+
+  const handleDescriptionChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setDescription(event.target.value)
+  }
+
+  const handleSearchQueryChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value
+    setSearchQuery(query)
+
+    const matchedUser = users.find(
+      (user) => user.userId.toLowerCase() === query.toLowerCase(),
+    )
+
+    const matchedUsers = users.filter((user) =>
+      user.userId.toLowerCase().includes(query.toLowerCase()),
+    )
+
+    setSearchResults(matchedUsers.slice(0, 5))
+
+    if (matchedUser && !selectedMembers.includes(matchedUser)) {
+      setSelectedMembers((prevMembers) => [...prevMembers, matchedUser])
+    }
+  }
+
+  const handleCreateChannel = () => {
+    const channelData = {
+      name: channelName,
+      description,
+      members: selectedMembers.map((member) => member.userId),
+      workspace: router.query.classCode,
+    }
+
+    // To Do: token 가져오는 방식
+    fetch(`${localPort}/channels/`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        token:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzM3MTYwZjJjNzNkMDgzMTgxNGM2ZiIsInR5cGUiOiJTVFVERU5UIiwiaWF0IjoxNjg1MzIzMTkyLCJleHAiOjE2ODU0MDk1OTJ9.2bSfViIASj6K-9DGppFiZBlo0s3PSK70kdPxDibYb6g',
+      },
+      body: JSON.stringify(channelData),
+    }).then((response) => {
+      if (response.ok) {
+        console.log('response.ok')
+        router.push(`/channel/${channelData.name}`)
+      } else {
+        console.log('not response.ok')
+      }
+    })
+  }
+
   return (
     <SideBar>
       <Stack sx={{ paddingTop: 4 }} spacing={2}>
@@ -30,8 +94,10 @@ export default function Channel() {
           </Typography>
           <TextField
             fullWidth
-            placeholder={dummyData.workspaceNamePlaceholder}
+            placeholder={placeholder.channelName}
             variant="standard"
+            value={channelName}
+            onChange={handleChannelNameChange}
           />
         </Box>
         <Box>
@@ -40,44 +106,55 @@ export default function Channel() {
           </Typography>
           <TextField
             fullWidth
-            placeholder={dummyData.descriptionPlaceholder}
+            placeholder={placeholder.description}
             variant="standard"
+            value={description}
+            onChange={handleDescriptionChange}
           />
         </Box>
         <Box>
           <Grid container spacing={2}>
             <Grid item xs={10}>
               <Typography variant="h5">
-                Members ({dummyData.members.length})
+                Members ({selectedMembers.length})
               </Typography>
             </Grid>
             <Grid item xs={2}>
-              <SearchBar />
+              <SearchBar onChange={handleSearchQueryChange} />
             </Grid>
           </Grid>
-          <List
-            dense
-            sx={{
-              pl: 2,
-            }}
-          >
-            {dummyData.members.map((value) => {
-              const labelId = `member-${value}`
+          <List dense sx={{ pl: 2 }}>
+            {selectedMembers.map((value) => {
+              const labelId = `member-${value.userId}`
               return (
-                <ListItem key={value} disablePadding>
+                <ListItem key={value.userId} disablePadding>
                   <ListItem>
-                    <ListItemAvatar>
-                      <Avatar
-                        alt={`Avatar ${value}`}
-                        src={`/static/images/avatar/${value}.jpg`}
-                      />
-                    </ListItemAvatar>
-                    <ListItemText id={labelId} primary={`${value}`} />
+                    <ListItemText id={labelId} primary={`${value.userId}`} />
                   </ListItem>
                 </ListItem>
               )
             })}
           </List>
+          {searchResults.length > 0 && (
+            <Box mt={4}>
+              <Typography variant="h6">Search Results</Typography>
+              <List dense sx={{ pl: 2 }}>
+                {searchResults.map((value) => {
+                  const labelId = `member-${value.userId}`
+                  return (
+                    <ListItem key={value.userId} disablePadding>
+                      <ListItem>
+                        <ListItemText
+                          id={labelId}
+                          primary={`${value.userId}`}
+                        />
+                      </ListItem>
+                    </ListItem>
+                  )
+                })}
+              </List>
+            </Box>
+          )}
         </Box>
         <Box
           sx={{
@@ -92,6 +169,7 @@ export default function Channel() {
               width: 300,
               height: 50,
             }}
+            onClick={handleCreateChannel}
           >
             Create
           </Button>
@@ -99,4 +177,20 @@ export default function Channel() {
       </Stack>
     </SideBar>
   )
+}
+
+export async function getServerSideProps() {
+  const options = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const res = await fetch(`${localPort}/users/`, options)
+  const users = await res.json()
+
+  return {
+    props: { users },
+  }
 }
