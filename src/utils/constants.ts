413f3292eef5c745c87085c8f1c89cda6ed63d82
yumin-jung <jungym887@naver.com>
2023-05-29 15:17:04 +0900

    feat: Update placeholder in constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,6 @@ export const localPort = 'http://localhost:8080'
 
 export const placeholder = {
   workspaceName: 'Enter Workspace Name',
+  channelName: 'Enter Channel Name',
   description: 'Enter Workspace Description',
 }
