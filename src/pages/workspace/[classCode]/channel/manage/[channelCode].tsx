fe690f69dd5da5a5d172d1d166ddc1176ad83458
yumin-jung <jungym887@naver.com>
2023-06-02 12:35:32 +0900

    feat: Update API in channelManage page

--- src/pages/workspace/[classCode]/channel/manage/[channelCode].tsx
+++ src/pages/workspace/[classCode]/channel/manage/[channelCode].tsx
@@ -9,42 +9,107 @@ import Button from '@mui/material/Button'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
-import ListItemAvatar from '@mui/material/ListItemAvatar'
-import Avatar from '@mui/material/Avatar'
 import Checkbox from '@mui/material/Checkbox'
-import SearchBar from '@/components/SearchBar'
-import { IChannel } from '@/utils/types'
+import { IUser, IWorkspace, IChannel } from '@/utils/types'
 import { localPort } from '@/utils/constants'
+import TextField from '@mui/material/TextField'
 import SideBar from '../../../../../components/SideBar'
 
 interface ChannelProps {
+  users: IUser[]
+  workspaces: IWorkspace[]
   channels: IChannel[]
 }
 
-export default function ChannelComp({ channels }: ChannelProps) {
+export default function ChannelComp({
+  users,
+  workspaces,
+  channels,
+}: ChannelProps) {
   const router = useRouter()
-  const [, setSearchQuery] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+  const [searchMemberModalOpen, setSearchMemberModalOpen] = useState(false)
+  const [searchMemberResults, setMemberSearchResults] = useState<IUser[]>([])
+
+  // To Do: members 값 초기값 넣어주기
+  const curChannel = channels.filter((e) => {
+    // eslint-disable-next-line no-underscore-dangle
+    return e._id === router.query.channelCode
+  })[0]
+
+  const [selectedMembers, setSelectedMembers] = useState(curChannel.members)
 
   const handleSearchQueryChange = (event: ChangeEvent<HTMLInputElement>) => {
     const query = event.target.value
     setSearchQuery(query)
+
+    const matchedUser = users.find(
+      (user) => user.userId.toLowerCase() === query.toLowerCase(),
+    )
+
+    const matchedUsers = users.filter((user) =>
+      user.userId.toLowerCase().includes(query.toLowerCase()),
+    )
+
+    setMemberSearchResults(matchedUsers.slice(0, 5))
+
+    if (matchedUser && !selectedMembers.includes(matchedUser)) {
+      setSelectedMembers((prev) => [...prev, matchedUser])
+    }
+  }
+
+  const handleAddMember = (member: IUser) => {
+    if (!selectedMembers.includes(member)) {
+      setSelectedMembers((prev) => [...prev, member])
+    }
+  }
+
+  const handleKickMember = (member: IUser) => {
+    // To Do: delete member 구현
+    fetch(`${localPort}/channels/${router.query.channelCode}/members/kick`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        // To Do: token 얻는 방법 바꾸기
+        token:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzhhZjM3ODJmZjQyYmYyZTA2NGRmOSIsInR5cGUiOiJTVFVERU5UIiwiaWF0IjoxNjg1Njc0OTg4LCJleHAiOjE2ODU3NjEzODh9.sgKBUMfkp3kuacOuJhvq_zElCgjDK_4S1AufAY1hCQg',
+      },
+      // To Do: worskapce 바꾸기
+      body: JSON.stringify({ members: [member] }),
+    }).then((response) => {
+      if (response.ok) {
+        console.log('response.ok')
+        // router.push(`/workspace/${router.query.classCode}`)
+      } else {
+        console.log('not response.ok')
+      }
+    })
+  }
+
+  const openMemberSearchModal = () => {
+    setSearchMemberModalOpen(true)
+  }
+
+  const closeMemberSearchModal = () => {
+    setSearchMemberModalOpen(false)
   }
 
   const handleDeleteChannel = () => {
-    // To Do: token 바꾸기
     fetch(`${localPort}/channels/`, {
       method: 'DELETE',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
+        // To Do: token 얻는 방법 바꾸기
         token:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzM3MTYwZjJjNzNkMDgzMTgxNGM2ZiIsInR5cGUiOiJTVFVERU5UIiwiaWF0IjoxNjg1MzI2OTk3LCJleHAiOjE2ODU0MTMzOTd9.A9vZOlUogSWpsD139orcTyHNVJCjow5Hg5Pb7xp0sZY',
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzhhZjM3ODJmZjQyYmYyZTA2NGRmOSIsInR5cGUiOiJTVFVERU5UIiwiaWF0IjoxNjg1NjMwODI5LCJleHAiOjE2ODU3MTcyMjl9.ANaWZVXOG1TilUVDijDv8SXhsSV4AMXekqE7F_YUGWE',
       },
-      body: JSON.stringify(channels[0].name),
+      // To Do: worskapce 바꾸기
+      body: JSON.stringify({ name: channels[0].name, workspace: 'workspace' }),
     }).then((response) => {
       if (response.ok) {
-        console.log('response.ok')
-        // router.push('/workspace')
+        router.push(`/workspace/${router.query.classCode}`)
       } else {
         console.log('not response.ok')
       }
@@ -71,13 +136,17 @@ export default function ChannelComp({ channels }: ChannelProps) {
                 alignItems: 'center',
               }}
             >
-              <Checkbox checked />
+              {curChannel.name === 'Default Channel' ? (
+                <Checkbox defaultChecked />
+              ) : (
+                <Checkbox disabled />
+              )}
               <Typography variant="body2">Default Channel</Typography>
             </Box>
           </Grid>
           <Grid item xs={2}>
             <Button variant="text" onClick={handleDeleteChannel}>
-              Delete Workspace
+              Delete Channel
             </Button>
           </Grid>
         </Grid>
@@ -91,15 +160,22 @@ export default function ChannelComp({ channels }: ChannelProps) {
           <Typography sx={{ paddingBottom: 2 }} variant="h5">
             Description
           </Typography>
-          <Typography variant="body1">{channels[0].description}</Typography>
+          <Typography variant="body1">{curChannel.description}</Typography>
         </Box>
         <Box>
           <Grid container spacing={2}>
-            <Grid item xs={10}>
+            <Grid item xs={8.8}>
               <Typography variant="h5">Members</Typography>
             </Grid>
-            <Grid item xs={2}>
-              <SearchBar onChange={handleSearchQueryChange} />
+            <Grid item xs={3.2}>
+              <Button
+                variant="outlined"
+                onClick={openMemberSearchModal}
+                disableRipple
+                sx={{ width: 300 }}
+              >
+                Add member
+              </Button>
             </Grid>
           </Grid>
           <List
@@ -108,41 +184,114 @@ export default function ChannelComp({ channels }: ChannelProps) {
               pl: 2,
             }}
           >
-            {channels[0].members.map((value) => {
-              const labelId = `member-${value}`
-              return (
-                <ListItem key={labelId} disablePadding>
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar
-                        alt={`Avatar ${value}`}
-                        src={`/static/images/avatar/${value}.jpg`}
-                      />
-                    </ListItemAvatar>
-                    <ListItemText id={labelId} primary={`${value}`} />
-                  </ListItem>
-                </ListItem>
-              )
-            })}
+            <List dense sx={{ pl: 2 }}>
+              {selectedMembers &&
+                selectedMembers.map((value) => {
+                  const label = `member-${value}`
+                  return (
+                    <ListItem key={label} disablePadding>
+                      <ListItemText id={label} primary={`${value}`} />
+                      <Button onClick={() => handleKickMember(value)}>
+                        Delete
+                      </Button>
+                    </ListItem>
+                  )
+                })}
+            </List>
           </List>
         </Box>
       </Stack>
+      {/* Search Modal */}
+      {searchMemberModalOpen && (
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            background: 'rgba(0, 0, 0, 0.5)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            zIndex: 9999,
+          }}
+        >
+          <div
+            style={{
+              background: '#fff',
+              padding: 20,
+              borderRadius: 4,
+            }}
+          >
+            {/* Modal content goes here */}
+            <TextField
+              fullWidth
+              placeholder="Search members"
+              variant="standard"
+              value={searchQuery}
+              onChange={handleSearchQueryChange}
+            />
+
+            <List dense>
+              {searchMemberResults.map((value) => {
+                const labelId = `member-${value.userId}`
+                return (
+                  <ListItem
+                    key={value.userId}
+                    disablePadding
+                    button
+                    onClick={() => handleAddMember(value)}
+                  >
+                    <ListItemText id={labelId} primary={`${value.userId}`} />
+                  </ListItem>
+                )
+              })}
+            </List>
+
+            <Button onClick={closeMemberSearchModal}>Close</Button>
+          </div>
+        </div>
+      )}
     </SideBar>
   )
 }
 
 export async function getServerSideProps() {
-  const options = {
+  const workspacesOptions = {
     method: 'GET',
     headers: {
       Accept: 'application/json',
     },
   }
 
-  const res = await fetch(`${localPort}/channels/`, options)
-  const channels = await res.json()
+  const workspacesRes = await fetch(
+    `${localPort}/workspaces/`,
+    workspacesOptions,
+  )
+  const workspaces = await workspacesRes.json()
+
+  const usersOptions = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const usersRes = await fetch(`${localPort}/users/`, usersOptions)
+  const users = await usersRes.json()
+
+  const channelOptions = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const channelRes = await fetch(`${localPort}/channels/`, channelOptions)
+  const channels = await channelRes.json()
 
   return {
-    props: { channels },
+    props: { users, workspaces, channels },
   }
 }
